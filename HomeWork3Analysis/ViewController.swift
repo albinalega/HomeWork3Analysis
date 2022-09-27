72cb7a931c737f1aaa05c18a72581c768e9fd390
Albina <albina.exe@mail.ru>
2022-09-27 19:16:12 +0200

    Initial Commit

---
+++ HomeWork3Analysis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork3Analysis
+//
+//  Created by Альбина Лега on 27/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
